a6801d8e62f04c9321ee9096fcd3634fb3ff225f
jinia91 <creee77@gmail.com>
2024-08-17 15:13:50 +0900

    init

---
+++ src/main/kotlin/com/network/ncp_cd_test/NcpCdTestApplication.kt
@@ -0,0 +1,11 @@
+package com.network.ncp_cd_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NcpCdTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<NcpCdTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/network/ncp_cd_test/NcpCdTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.network.ncp_cd_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NcpCdTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
